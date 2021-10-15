11e8be09e971573dc26844fe6a91b1881f9d815b
Sam <smnaugle@berkeley.edu>
2021-10-15 15:08:07 -0400

    Fixing error when getting cupy array from bin_centers due to it being a list when defined

--- kdfit/signal.py
+++ kdfit/signal.py
@@ -92,7 +92,7 @@ class KernelDensityPDF(Signal):
             self.bin_edges = binning_to_edges(bootstrap_binning,lows=observables.lows,highs=observables.highs)
             self.indexes = [np.arange(len(edges)) for edges in self.bin_edges]
             self.a_kj, self.b_kj = edges_to_points(self.bin_edges)
-            self.bin_centers = [(edges[:-1]+edges[1:])/2 for edges in self.bin_edges]
+            self.bin_centers = cp.asarray([(edges[:-1]+edges[1:])/2 for edges in self.bin_edges])
             self.bin_vol = cp.ascontiguousarray(cp.prod(self.b_kj-self.a_kj,axis=1))
         self.reflect_axes = reflect_axes if reflect_axes is not None else [False for _ in range(len(observables.dimensions))]
         self.a = cp.asarray([l for l in observables.lows])
@@ -205,6 +205,7 @@ class KernelDensityPDF(Signal):
             #should do this on GPU...
             x_kj = cp.asnumpy(x_kj)
             from scipy.interpolate import RegularGridInterpolator
+            
             interp = RegularGridInterpolator(cp.asnumpy(self.bin_centers),cp.asnumpy(self.counts),bounds_error=False,fill_value=None)
             pdf_k = cp.asarray(interp(x_kj))
             min_val = np.min(self.counts)
